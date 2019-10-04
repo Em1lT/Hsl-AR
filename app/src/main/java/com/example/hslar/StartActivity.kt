9818b00bf5349cee4a513c53f6cd5c1611dc8889
Emil Toivainen <emil.toivainen@devoca.fi>
2019-10-04 16:24:56 +0300

    Added Start windows to choose from citybike or hsl

---
+++ app/src/main/java/com/example/hslar/StartActivity.kt
@@ -0,0 +1,28 @@
+package com.example.hslar
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_start.*
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+
+        hsl.setOnClickListener {
+            startActivity(MainActivity())
+        }
+
+        cityBikes.setOnClickListener {
+            startActivity(CBActivity())
+        }
+    }
+    private fun startActivity(activity: Activity){
+        val intent = Intent(this, activity::class.java)
+        startActivity(intent)
+    }
+}
